114939c7cf2fe9a7d9d5ef4495f017b2cb0b76eb
Sebastian Popa <popa.z.sebastian@gmail.com>
2020-12-23 13:35:44 +0200

    Add FDCAN1 hardware
    Initialized the FDCAN1 driver.
    FDCAN is configured as classic CAN format.
    FDCAN clock speed is 80 MHz = 160Mhz / 2
    Bitrate 1MB/s.
    Initlaised 10 StdId filters.
    Interrupts Enabled.

--- NG474-BOOT/Core/Src/main.c
+++ NG474-BOOT/Core/Src/main.c
@@ -19,6 +19,7 @@
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
+#include "fdcan.h"
 #include "gpio.h"
 
 /* Private includes ----------------------------------------------------------*/
@@ -85,6 +86,7 @@ int main(void)
 
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
+  MX_FDCAN1_Init();
   /* USER CODE BEGIN 2 */
   bootloader_JumpToUserApp();
   /* USER CODE END 2 */
