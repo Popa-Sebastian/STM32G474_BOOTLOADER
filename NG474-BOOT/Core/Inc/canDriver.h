9cab177bb08487b51dbf44a6cbfbfbd8012a09f9
Sebastian Popa <popa.z.sebastian@gmail.com>
2020-12-24 17:32:25 +0200

    Implement a check for received data to be in correct order
    After getting the message from FIFO0 the received index is extracted
    from the Rx Identifier. If the received index is different from the
    expected index, an error is sent on CAN bus:
    ID = 0x2FF;
    Data length 3
    1st Byte = 0xFF -error notificaiton
    2nd Byte = expected index
    3rd Byte = received index.
    
    Note: the expected index remains the same. If host application sends
    the correct data frame next, the procces can continue undisturbed.
    
    Clean function and create small ACK functions that send messages.

--- NG474-BOOT/Core/Inc/canDriver.h
+++ NG474-BOOT/Core/Inc/canDriver.h
@@ -30,6 +30,46 @@
 void can_init(void);
 
 /**
+  * @brief	Sends ack page complete frame
+  * 		ID = 0x300,
+  * 		DATA[1]
+  * 		[1]: 0x00
+  * @param	None
+  * @retval	None
+  */
+void can_ack_page_complete(void);
+
+/**
+  * @brief	Sends ack flash complete frame
+  * 		ID = 0x400,
+  * 		DATA[1]
+  * 		[1]: 0xFF
+  * @param	None
+  * @retval	None
+  */
+void can_acK_flash_complete(void);
+
+/**
+  * @brief	Sends ack and an echo of the received data
+  * 		ID = 0x200 + data_index
+  * 		DATA[8] = RxData
+  * @param	None
+  * @retval	None
+  */
+void can_ack_echo_data(void);
+
+/**
+  * @brief	Sends a error frame if the received index is different than expected
+  * 		ID = 0x2FF,
+  * 		DATA[3]
+  * 		[1]: 0xFF
+  * 		[2]: expected index
+  * 		[3]: received index
+  * @param	None
+  * @retval	None
+  */
+void can_error_wrong_index(void);
+/**
   * @brief	Converts an array of 8 uint8_t elements to a uint64_t
   * @param	Array is an array of 8 uint8_t elements
   * @retval	uint64_t converted value
