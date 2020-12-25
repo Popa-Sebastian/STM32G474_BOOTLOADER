f218467b06541b9baec5c196ba9b9fa9c8ee12c1
Sebastian Popa <popa.z.sebastian@gmail.com>
2020-12-25 12:55:25 +0200

    Create dataConversion files to store array to data conversion funct

---
+++ NG474-BOOT/Core/Inc/dataConversion.h
@@ -0,0 +1,18 @@
+/**
+  ******************************************************************************
+  * @file           : dataConversion.h
+  * @brief          : Functions that convert from array elements do single data
+  * 				  element
+  * ----------------------------------------------------------------------------
+  * @author         : Sebastian Popa
+  * @date           : Dec, 2020
+  ******************************************************************************
+  */
+
+#ifndef INC_DATACONVERSION_H_
+#define INC_DATACONVERSION_H_
+
+/* Private function prototypes -----------------------------------------------*/
+
+
+#endif /* INC_DATACONVERSION_H_ */
