81a8a21ae3904a0a5eb92b2611cf6c2878ab50e3
Sebastian Popa <popa.z.sebastian@gmail.com>
2020-12-25 11:55:50 +0200

    Create canReplyMsg files
    Created the *.c and *.h files to store functions related to
    replies sent to HOST to indicate status of operations.
    
    These files will unclutter the caDriver files removing
    unessential functions.

---
+++ NG474-BOOT/Core/Inc/canReplyMsg.h
@@ -0,0 +1,16 @@
+/**
+  ******************************************************************************
+  * @file           : canReplyMsg.h
+  * @brief          : Reply messages send to host to indicate status
+  * ----------------------------------------------------------------------------
+  * @author         : Sebastian Popa
+  * @date           : Dec, 2020
+  ******************************************************************************
+  */
+#ifndef INC_CANREPLYMSG_H_
+#define INC_CANREPLYMSG_H_
+
+/* Private function prototypes -----------------------------------------------*/
+
+
+#endif /* INC_CANREPLYMSG_H_ */
