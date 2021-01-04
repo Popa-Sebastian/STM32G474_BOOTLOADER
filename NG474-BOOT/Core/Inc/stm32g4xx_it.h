f05e4b2fdb678dbc66f2516fa0a0017e9d090690
Sebastian Popa <popa.z.sebastian@gmail.com>
2021-01-04 08:26:07 +0200

    Implement timer_16
    Timer_16 will be used to count elapsed time from mcu reset. During
    this countdown bootloader app will wait for a HOST_ENTER_BOOTLOADER
    command received from CAN bus. If this command is not received in 5
    seconds, the bootloader will jump automatically to the already stored
    user_app.
    
    Timer_16 parameters:
    	prescaler: 160 ( clock freq = 160Mhz, makes timer_16 clock 1Mhz)
    	reload_val: 4999 (timer period is 5s)
    
    NOTE: files generated automatically by CubeMX modify (c) date to 2021

--- NG474-BOOT/Core/Inc/stm32g4xx_it.h
+++ NG474-BOOT/Core/Inc/stm32g4xx_it.h
@@ -57,6 +57,7 @@ void DebugMon_Handler(void);
 void PendSV_Handler(void);
 void SysTick_Handler(void);
 void FDCAN1_IT0_IRQHandler(void);
+void TIM1_UP_TIM16_IRQHandler(void);
 void EXTI15_10_IRQHandler(void);
 /* USER CODE BEGIN EFP */
 
