741a632e76995c983ded02a9fd3603d01e95cfe6
Sebastian Popa <popa.z.sebastian@gmail.com>
2021-01-04 15:35:21 +0200

    Implement HOST_USER_ADDRESS mode in can_host_handler
    When HOST_USER_ADDRESS (0x10) command is received, the data payload
    is coverted into a uin32_t address and stored in flash_address.
    
    function array_to_uint32() created and added to dataConversion.c/.h
    based on array_to_uint64() function already present.

--- NG474-BOOT/Core/Src/dataConversion.c
+++ NG474-BOOT/Core/Src/dataConversion.c
@@ -31,5 +31,23 @@ uint64_t array_to_uint64(uint8_t *Array)
 	return converted_value;
 }
 
+/* Functions declaration- ----------------------------------------------------*/
+/**
+  * @brief	Converts an array of 4 uint8_t elements to a uint32_t
+  * @param	Array is an array of 4 uint8_t elements
+  * @retval	uint32_t converted value
+  */
+uint64_t array_to_uint32(uint8_t *Array)
+{
+	int array_index;
+	uint32_t converted_value = 0;
+	for (array_index = 0; array_index < 3; array_index++)
+	{
+	  converted_value += Array[array_index];
+	  converted_value = (converted_value << 8);
+	}
+	converted_value += Array[3];
+	return converted_value;
+}
 /***************************END OF FILE****************************************/
 
