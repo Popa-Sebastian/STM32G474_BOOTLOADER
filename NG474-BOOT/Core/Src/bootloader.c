a7157cf13bfe3523ae33bd92cd29b49a73a7f650
Sebastian Popa <popa.z.sebastian@gmail.com>
2021-01-04 17:25:24 +0200

    Non - functional: commentary (added title to function commentary)

--- NG474-BOOT/Core/Src/bootloader.c
+++ NG474-BOOT/Core/Src/bootloader.c
@@ -16,7 +16,8 @@
 
 
 /* Functions declaration- ----------------------------------------------------*/
-/**
+/*********************bootloader_JumpToUserApp**********************************
+ **
   * @brief  Jumps to the user application located at a specified memory location.
   *         Gets the user app Main Stack Pointer (MSP) and the address of the
   *         user app reset_handler() function.
@@ -46,7 +47,8 @@ void bootloader_JumpToUserApp(void)
 	user_reset_handler();
 }
 
-/**
+/*********************bootloader_FlashEraseBank2********************************
+ **
   * @brief	This function erases Bank2 of the memory. (page 128 - page 255)
   * @param 	None
   * @retval	HAL_FLASH_GetError
@@ -72,8 +74,9 @@ uint32_t bootloader_FlashEraseBank2(void)
 	return Status;
 }
 
-/**
-  * @brief	This function erases a number of pages starting from the specifed
+/*********************bootloader_FlashErasePage*********************************
+ **
+  * @brief	This function erases a number of pages starting from the specified
   * 		page number
   * @param 	Page, starting page to begin erase
   * @param  Numberpages, how many pages to be erased
@@ -97,7 +100,8 @@ uint32_t bootloader_FlashErasePage(uint32_t Page, uint32_t NumberPages)
 	return Status;
 }
 
-/**
+/*********************bootloader_FlashWrite*************************************
+ **
   * @brief	This function writes 2KB of data starting from a given address
   * @param	StartAddress is the starting address from where to start the flash write
   * @param	DATA_64 is a pointer to an array of data to be written of size (32 x 64bit)
