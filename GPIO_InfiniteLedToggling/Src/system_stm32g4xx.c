e5fbbbdcb9b23b5f5816e678bd0f76bb5bc62134
Sebastian Popa <popa.z.sebastian@gmail.com>
2021-01-05 10:55:23 +0200

    Modify user flash area to 0x08040000 (for testing purposes)
    modifying user starting flash area to second memory bank makes deleting
    data faster and easier in this phase of the project.

--- GPIO_InfiniteLedToggling/Src/system_stm32g4xx.c
+++ GPIO_InfiniteLedToggling/Src/system_stm32g4xx.c
@@ -106,7 +106,7 @@
 /*!< Uncomment the following line if you need to relocate your vector Table in
      Internal SRAM. */
 /* #define VECT_TAB_SRAM */
-#define VECT_TAB_OFFSET  0x8000UL /*!< Vector Table base offset field.
+#define VECT_TAB_OFFSET  0x40000UL /*!< Vector Table base offset field.
                                    This value must be a multiple of 0x200. */
 /******************************************************************************/
 /**
