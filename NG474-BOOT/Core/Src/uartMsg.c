a6b4555b5d642c4f6726acc01608457b146fcbae
Sebastian Popa <popa.z.sebastian@gmail.com>
2021-01-08 15:24:06 +0200

    Create uartMsg.c/.h files and uart_send_msg() function
    uart_send_msg() sends a String through USART1 connected to virtual COM port
    
    Messages can be received using a terminal progra, (such as Putty)

---
+++ NG474-BOOT/Core/Src/uartMsg.c
@@ -0,0 +1,34 @@
+/**
+  ******************************************************************************
+  * @file           : uartMsg.c
+  * @brief          : Functions to send messages through USART1
+  * ----------------------------------------------------------------------------
+  * @author         : Sebastian Popa
+  * @date           : Jan, 2021
+  ******************************************************************************
+  */
+
+/* Includes ------------------------------------------------------------------*/
+#include <stdio.h>
+#include "main.h"
+#include "uartMsg.h"
+
+/* Variables declared elsewhere-----------------------------------------------*/
+extern UART_HandleTypeDef huart1; //
+
+/* Private function prototypes -----------------------------------------------*/
+/**************************uart_send_msg****************************************
+ **
+  * @brief	Sends a message through UART
+  * @param	user_data
+  * @retval	status
+  */
+HAL_StatusTypeDef uart_send_msg(char *user_data)
+{
+	HAL_StatusTypeDef Status;
+	Status = HAL_UART_Transmit(&huart1, (uint8_t*)user_data, strlen(user_data), UART_DELAY);
+
+	return Status;
+}
+
+/***************************END OF FILE****************************************/
