48e132653542518b73ad271fe8ea95d636be5667
Sebastian Popa <popa.z.sebastian@gmail.com>
2021-01-11 10:28:19 +0200

    Add the uartMsg.c/.h files to handle UART messages

--- UART_Hello/Core/Src/main_uart.c
+++ UART_Hello/Core/Src/main_uart.c
@@ -22,7 +22,7 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-
+#include "uartMsg.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
