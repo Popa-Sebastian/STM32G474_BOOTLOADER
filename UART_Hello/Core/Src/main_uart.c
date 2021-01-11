e2d2ede9e28dbcdf3c3fec53433c1bb30e4dc87f
Sebastian Popa <popa.z.sebastian@gmail.com>
2021-01-11 12:08:54 +0200

    Modified doc files to accomodate new commands available from host

--- UART_Hello/Core/Src/main_uart.c
+++ UART_Hello/Core/Src/main_uart.c
@@ -66,7 +66,6 @@ static void MX_USART1_UART_Init(void);
 int main(void)
 {
   /* USER CODE BEGIN 1 */
-
   /* USER CODE END 1 */
 
   /* MCU Configuration--------------------------------------------------------*/
